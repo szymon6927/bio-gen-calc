4d2a676bebf3e32416d99e89356d5387dd4646ac
Szymon Miks <miks.szymon@gmail.com>
2018-06-30 16:11:43 +0200

    remove redirection form app.js and add in head section in base.html even before google analitycs

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -4,12 +4,6 @@ particlesJS.load('particles-js', '/static/js/assets/particles.json', function ()
 });
 
 $(document).ready(function () {
-
-  var currentUrl = window.location.href;
-  if (currentUrl.includes("herokuapp.com")) {
-    window.location.replace("https://gene-calc.pl/");
-  }
-
   $("#back-top").hide();
   $(function () {
     $(window).scroll(function () {
