d76a42dfcfb3cac57f81a083c6d4cfc81f3d64dc
Szymon Miks <szymonm@miroart.pl>
2018-07-11 15:57:14 +0200

    add function to prevent from insert negative number, add class for this to input, change padding on inputs

--- app/hardy_weinber/forms.py
+++ app/hardy_weinber/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, SelectField, SubmitField, ValidationError, validators
+from flask.ext.wtf.html5 import NumberInput
 from wtforms.validators import InputRequired
 
 
@@ -7,22 +8,10 @@ class HardyWeinberForm(FlaskForm):
     """
     Form for HardyWeinberForm calculation
     """
-    ho = IntegerField('ho', validators=[InputRequired(message="Podaj ho")])
-    he = IntegerField('he', validators=[InputRequired(message="Podaj he")])
-    rho = IntegerField('rho', validators=[InputRequired(message="Podaj rho")])
+    ho = IntegerField('ho', validators=[InputRequired(message="Podaj ho")], widget=NumberInput(min=0), )
+    he = IntegerField('he', validators=[InputRequired(message="Podaj he")], widget=NumberInput(min=0))
+    rho = IntegerField('rho', validators=[InputRequired(message="Podaj rho")], widget=NumberInput(min=0))
     critical_select = SelectField(u'Programming Language',
                                   choices=[('1', '0.05'), ('2', '0.01')],
                                   validators=[InputRequired(message="Wybierz stopień istoności")])
     submit = SubmitField('Calcuate!')
-
-    # def validate_ho(self, field):
-    #     if field.data < 0:
-    #         raise ValidationError('Field can not be a 0 value')
-    #
-    # def validate_he(self, field):
-    #     if field.data < 0:
-    #         raise ValidationError('Field can not be a 0 value')
-    #
-    # def validate_rho(self, field):
-    #     if field.data < 0:
-    #         raise ValidationError('Field can not be a 0 value')
---
+++ app/static/js/utils.js
@@ -0,0 +1,27 @@
+var inputs = document.querySelectorAll(".non-negative");
+
+function keyAllowed(key) {
+  var keys = [8, 9, 13, 16, 17, 18, 19, 20, 27, 46, 48, 49, 50,
+    51, 52, 53, 54, 55, 56, 57, 91, 92, 93
+  ];
+  if (key && keys.indexOf(key) === -1)
+    return false;
+  else
+    return true;
+}
+
+inputs.forEach(function (element) {
+
+  element.addEventListener('keypress', function (e) {
+    var key = !isNaN(e.charCode) ? e.charCode : e.keyCode;
+    if (!keyAllowed(key))
+      e.preventDefault();
+  }, false);
+
+  // Disable pasting of non-numbers
+  element.addEventListener('paste', function (e) {
+    var pasteData = e.clipboardData.getData('text/plain');
+    if (pasteData.match(/[^0-9]/))
+      e.preventDefault();
+  }, false);
+})
