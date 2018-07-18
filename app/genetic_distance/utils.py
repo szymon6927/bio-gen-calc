8232fb7a30ede9fed1301418784107835cc24db3
Szymon Miks <miks.szymon@gmail.com>
2018-07-18 18:40:22 +0200

    resolve problem with matplotlib on backend server, add important inport at top of genetic_distance/utils.py which tell matplotlib to use AGG, add offset to dendrogram result wrapper in html, genere new requirements with the newest version of matplot lib

--- app/genetic_distance/utils.py
+++ app/genetic_distance/utils.py
@@ -1,3 +1,10 @@
+# important for use matplotlib at backend
+import os
+import matplotlib as mpl
+if os.environ.get('DISPLAY','') == '':
+    print('no display found. Using non-interactive Agg backend')
+    mpl.use('Agg')
+
 import numpy as np
 import itertools
 from math import sqrt, log
