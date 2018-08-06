913f78d8cf75e6270e1be4300a3fb0894880ebd3
Szymon Miks <szymonm@miroart.pl>
2018-08-06 15:35:58 +0200

    add Testing config

--- config.py
+++ config.py
@@ -25,7 +25,15 @@ class ProductionConfig(Config):
     DEBUG = False
 
 
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+    TESTING = True
+
+
 app_config = {
     'development': DevelopmentConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'testing': TestingConfig
 }
