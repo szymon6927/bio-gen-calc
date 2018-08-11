436500ca20b8d237b4de655fc65915371760947c
Szymon Miks <miks.szymon@gmail.com>
2018-08-11 23:41:34 +0200

    add statement if error container is hideen show them

--- app/static/js/helpers/RenderHelper.js
+++ app/static/js/helpers/RenderHelper.js
@@ -11,34 +11,38 @@ class RenderHelper {
         </div>
       </div>`;
 
-      let transform = {
-        '<>': 'div', 'class':'row result-score', 'html': [
-          {
-            '<>': 'div', 'class': 'col-md-6 col-sm-6 col-12 text-left result-name',
-            'html': '<div>${name} =</div>',
-          },
-          {
-            '<>': 'div', 'class': 'col-md-6 col-sm-6 col-12 text-left result-value',
-            'html': '<div>${value}</div>',
-          }
-        ]
-      };
+    let transform = {
+      '<>': 'div', 'class': 'row result-score', 'html': [
+        {
+          '<>': 'div', 'class': 'col-md-6 col-sm-6 col-12 text-left result-name',
+          'html': '<div>${name} =</div>',
+        },
+        {
+          '<>': 'div', 'class': 'col-md-6 col-sm-6 col-12 text-left result-value',
+          'html': '<div>${value}</div>',
+        }
+      ]
+    };
 
-      let pdfRender = `<div class="btn btn-secondary mt-3 render-to-pdf">
+    let pdfRender = `<div class="btn btn-secondary mt-3 render-to-pdf">
                           <i class="fas fa-file-pdf"></i>
                           <span>Save as PDF</span>
                       </div>`;
 
-      let date = moment().format('DD/MM/YYYY-hh:mm');
-      let downloadButton = `<a id='download-link' download='results-${date}.pdf' style="display:none;" /> `;
+    let date = moment().format('DD/MM/YYYY-hh:mm');
+    let downloadButton = `<a id='download-link' download='results-${date}.pdf' style="display:none;" /> `;
 
-      $(this.container).html(template);
-      $(this.container + ' .card-body').json2html(result.data, transform);
-      $(this.container + ' .card-body').append(pdfRender);
-      $(this.container + ' .card-body').append(downloadButton);
+    $(this.container).html(template);
+    $(this.container + ' .card-body').json2html(result.data, transform);
+    $(this.container + ' .card-body').append(pdfRender);
+    $(this.container + ' .card-body').append(downloadButton);
   }
 
   errorBlock(request) {
+    if ($(this.container).is(":hidden")) {
+      $(this.container).show();
+    }
+
     let errMsg = '';
     if (request && (request.status === 409 || request.status === 400)) {
       errMsg = `<div><strong>Error message:</strong> ${request.responseText}</div>`;
