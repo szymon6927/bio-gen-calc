4f0d96ae156a87e1a3b7cb85c88c3c5750098ca2
Szymon Miks <szymonm@miroart.pl>
2018-08-14 16:10:25 +0200

    repiar iterating over avg dict

--- app/genetic_distance/utils/GeometricDistance.py
+++ app/genetic_distance/utils/GeometricDistance.py
@@ -6,7 +6,6 @@ from scipy.stats.mstats import gmean
 class GeometricDistance(GeneticDistance):
     def __init__(self, data):
         super().__init__(data)
-        self.column_geo_avg = dict()
 
     def calculate_square_sum(self):
         g_square_sum = dict()
@@ -24,19 +23,20 @@ class GeometricDistance(GeneticDistance):
                 square_sum = sum(j*j for j in g_list)
                 square_sum_list.append(square_sum)
 
-            print(f'{i} - {square_sum_list}', flush=True)
             g_square_sum[i] = square_sum_list
 
         return g_square_sum
 
     def calculate_geometric_avg(self):
+        column_geo_avg = dict()
         g_square_sum = self.calculate_square_sum()
 
-        for key, value_list in g_square_sum:
+        for key, value_list in g_square_sum.items():
             square_sum_list = g_square_sum[key]
-            self.column_geo_avg[key] = gmean(square_sum_list)
+            column_geo_avg[key] = gmean(square_sum_list)
+
+        return column_geo_avg
 
     def calcuate_distances(self):
-        print("test", flush=True)
-        self.calculate_geometric_avg()
-        print("Geomatric distane for columns", flush=True)
+        geo_avg = self.calculate_geometric_avg()
+        print(f'Geomatric avg for columns: {geo_avg}', flush=True)
