c6d6e53582996b6863d5f94ff01f49ef9dd76a40
Szymon Miks <szymonm@miroart.pl>
2018-10-05 14:41:01 +0200

    created form on contact page, but now it's only view with out mail sending

--- app/contact/views.py
+++ app/contact/views.py
@@ -1,7 +1,14 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from . import contact
+from .forms import ContactForm
 
 
-@contact.route('/contact')
+@contact.route('/contact', methods=['GET', 'POST'])
 def contact_page():
-    return render_template('contact/index.html', title="Contact Us")
+    contact_form = ContactForm()
+    if contact_form.validate_on_submit():
+        flash(f'Thanks <strong>{contact_form.name.data}</strong> for your message.')
+        return redirect(url_for('contact.contact_page'))
+    
+    return render_template('contact/index.html', title="Contact Us", form=contact_form)
+
