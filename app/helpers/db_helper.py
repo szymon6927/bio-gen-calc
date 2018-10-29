5ea42ccfa2c6206ac74b35662bd6c4a8d0330247
Szymon Miks <szymonm@miroart.pl>
2018-10-29 11:06:41 +0100

    in add_calculation function added special string to inform in calculation made in debug mode

--- app/helpers/db_helper.py
+++ app/helpers/db_helper.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from flask import current_app
 from ..database import db
 from ..models.Calculation import Calculation
 
@@ -8,6 +9,9 @@ def add_calculation(module_name=None, user_data=None, result=None, ip_address=No
     if module_name and user_data and result and ip_address:
         if type(result) is np.ndarray:
             result = result.tolist()
+
+        if current_app.debug:
+            module_name = "DEV_MODE_" + module_name
 
         calculation = Calculation(module_name=module_name, user_data=json.dumps(user_data), result=json.dumps(result),
                                   ip_address=ip_address)
