8031bb9ce38e41ba0e64774101e03dded1326533
Szymon Miks <miks.szymon@gmail.com>
2018-11-27 21:32:04 +0100

    imporeve functional tests

--- app/tests/functional/test_customer.py
+++ app/tests/functional/test_customer.py
@@ -1,16 +1,11 @@
 def test_valid_login_logout(test_client, init_db):
-    print(init_db)
     data = {
         "login_or_email": "test2@test.com",
         "password": "testing321"
     }
-    # response = test_client.post('/userpanel/login', data=dict(login_or_email='test1@test.com', password='testing123'), follow_redirects=True)
 
-    response = test_client.post('/userpanel/login',
-                                data=data,
-                                follow_redirects=True)
+    response = test_client.post('/userpanel/login', data=data, follow_redirects=True)
 
     assert response.status_code == 200
     assert b"Invalid username or password" not in response.data
     assert b"Welcome" in response.data
-    # assert b"Welcome" in response.data
