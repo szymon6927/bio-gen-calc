b636d03010c3e4f190b6a3dcabb671abb368f779
Szymon Miks <szymonm@miroart.pl>
2018-11-28 16:06:39 +0100

    chage module name

--- config.py
+++ config.py
@@ -2,7 +2,14 @@ class Config:
     """
     Common configurations
     """
-    MAX_CONTENT_LENGTH = 4 * 1024 * 1024
+    COMPRESS_MIMETYPES = ['text/html', 'text/css', 'text/xml', 'application/json', 'application/javascript']
+    COMPRESS_LEVEL = 6
+    COMPRESS_MIN_SIZE = 500
+
+    MINIFY_PAGE = True
+
+    MAX_CONTENT_LENGTH = 4 * 1024 * 1024 # for file upload
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = "mail26.mydevil.net"
     MAIL_PORT = 465
@@ -14,17 +21,12 @@ class DevelopmentConfig(Config):
     Development configurations
     """
     DEBUG = True
-    MINIFY_PAGE = True
 
 
 class ProductionConfig(Config):
     """
     Production configurations
     """
-    COMPRESS_MIMETYPES = ['text/html', 'text/css', 'text/xml', 'application/json', 'application/javascript']
-    COMPRESS_LEVEL = 6
-    COMPRESS_MIN_SIZE = 500
-    MINIFY_PAGE = True
     DEBUG = False
 
 
@@ -35,8 +37,10 @@ class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     # Bcrypt algorithm hashing rounds (reduced for testing purposes only!)
     BCRYPT_LOG_ROUNDS = 4
+    
     # Disable CSRF tokens in the Forms (only valid for testing purposes!)
     WTF_CSRF_ENABLED = False
 
