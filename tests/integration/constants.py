5f2c4e299581f0b48ede347b6d550197e34f79c6
Szymon Miks <szymon.miks@netguru.com>
2019-08-04 18:46:52 +0200

    [trello-28]: Added tests for contact page and contact form

--- tests/integration/constants.py
+++ tests/integration/constants.py
@@ -3,3 +3,5 @@ class URL:
     CHI_SQUARE_GET = "/chi-square-page"
     CHI_SQUARE_POST = "/chi-square/send-data"
     CHI_SQUARE_GOODNESS_POST = "/chi-square/send-data-goodness"
+    CONTACT_GET = "/contact"
+    CONTACT_POST = "/contact"
