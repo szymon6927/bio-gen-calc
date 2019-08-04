ebd07c7ca9d9b421b20916ee755d1de77f15c187
Szymon Miks <szymon.miks@netguru.com>
2019-08-04 17:44:56 +0200

    [trello-28]: Added constants.py with url mapping for tests

---
+++ tests/integration/constants.py
@@ -0,0 +1,5 @@
+class URL:
+    ABOUT_GET = "/about"
+    CHI_SQUARE_GET = "/chi-square-page"
+    CHI_SQUARE_POST = "/chi-square/send-data"
+    CHI_SQUARE_GOODNESS_POST = "/chi-square/send-data-goodness"
