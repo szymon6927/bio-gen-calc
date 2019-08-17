5c0ffa46a70ca15879f405605ee7a3aea15d7bd7
Janbinkowski96 <jan.binkowski96@gmail.com>
2019-08-17 23:10:11 +0200

    [trello-41]: Updated test, after update in dotplot

--- tests/unit/test_dotplot_module.py
+++ tests/unit/test_dotplot_module.py
@@ -44,8 +44,8 @@ def test_dotplot_genebank_basic():
     results = dotplot.genebank_seq()
 
     expected_results = [
-        {'name': "Seq id. Z78533.1 C.irapeanum 5.8S rRNA gene and ... lenght [bp]", 'value': 740},
-        {'name': "Seq id. Z78532.1 C.californicum 5.8S rRNA gene  ... lenght [bp]", 'value': 753},
+        {'name': "Seq id. Z78533.1 C.irapeanum 5.8S rRNA gen ... lenght [bp]", 'value': 740},
+        {'name': "Seq id. Z78532.1 C.californicum 5.8S rRNA  ... lenght [bp]", 'value': 753},
         {'name': "Coverage [%]", 'value': 98.3},
         {'name': "Average identity [%]", 'value': 82.4},
         {'name': "Fragmental identity [%]", 'value': 83.1},
