d5139e32ad0c4d04ddfec4a32266faa13890297c
Szymon Miks <szymon.miks@netguru.com>
2019-08-31 12:32:31 +0200

    [trello-16]: Added FE

--- app/static/js/ampc/AMPCEvents.js
+++ app/static/js/ampc/AMPCEvents.js
@@ -2,8 +2,14 @@
 
 const ampc = new AMPCModule();
 const preTrainForm = document.querySelector('#ml-pre-train-form');
+const trainButton = document.querySelector('.train');
 
 preTrainForm.addEventListener('submit', event => {
   event.preventDefault();
   ampc.preTrain();
+});
+
+trainButton.addEventListener('click', event => {
+  event.preventDefault();
+  ampc.train();
 });
--- app/static/js/ampc/AMPCModule.js
+++ app/static/js/ampc/AMPCModule.js
@@ -3,6 +3,61 @@
 class AMPCModule extends AppModule {
   constructor() {
     super();
+    this.ampcDataID = null;
+  }
+
+  setAmpcDataID(id) {
+    this.ampcDataID = id;
+  }
+
+  getAmpcDataID() {
+    return this.ampcDataID;
+  }
+
+  renderPreTrainResultsClassification(result) {
+    const wrapper = $('.pre-train-results');
+    let template = '';
+
+    result.forEach((item, index) => {
+      template += `
+      <div>Model name: ${item.model_name}</div>
+      <div>Accuracy: ${item.accuracy}</div>
+      <div>cross_validate_score: ${item.cross_validate_score}</div>
+      <div>${item.matrix_report}</div>`
+    });
+
+    wrapper.html(template);
+  }
+
+  renderPreTrainResultsRegression(result) {
+    const wrapper = $('.pre-train-results');
+    let template = '';
+
+    result.forEach((item, index) => {
+      template += `
+      <div class="mb-3">
+        <div>Model name: ${item.model_name}</div>
+        <div>cross_validate_score: ${item.cross_validate_score}</div>
+        <div>MAE: ${item.MAE}</div>
+        <div>MSE: ${item.MSE}</div>
+        <div>R2: ${item.R2}</div>
+      </div>`
+    });
+
+    wrapper.html(template);
+  }
+
+  renderModelChoices(modelChoices) {
+    const selectWraper = $('.user-model-choice select');
+    let choices = '';
+
+    modelChoices.forEach((item, index) => {
+      choices += `
+        <option value="${item.key}">${item.name}</option>
+      `
+    });
+
+    selectWraper.html(choices);
   }
 
   preTrain() {
@@ -24,7 +79,6 @@ class AMPCModule extends AppModule {
     formData.append('model_type', modelType);
     formData.append('normalization', normalization);
 
-
     $.ajax({
       type: "POST",
       url: path,
@@ -34,8 +88,19 @@ class AMPCModule extends AppModule {
       processData: false,
       success: (result) => {
         console.log("Succesfull");
-
         console.log(result);
+
+        this.setAmpcDataID(result.data_id);
+
+        if (modelType === "classification") {
+          this.renderPreTrainResultsClassification(result.model_metrics);
+        }
+        else {
+          this.renderPreTrainResultsRegression(result.model_metrics);
+        }
+
+        $('.user-model-choice').show();
+        this.renderModelChoices(result.user_choices);
 
         $('.cover').hide();
       },
@@ -48,5 +113,44 @@ class AMPCModule extends AppModule {
         $('.cover').hide();
       }
     });
+  }
+
+  buildTrainJSON() {
+    let data = {};
+
+    data['selected_model'] = document.querySelector('.user-model-choice select').value;
+    data['data_id'] = this.getAmpcDataID();
+
+    return JSON.stringify(data);
+  }
+
+  train() {
+    $('.cover').show();
+    const path = '/ampc/train';
+    const dataJSON = this.buildTrainJSON();
+    const render = new RenderHelper('.train-errors');
+
+    $.ajax({
+      type: "POST",
+      contentType: "application/json; charset=utf-8",
+      url: path,
+      data: dataJSON,
+      dataType: "json",
+      success: (result) => {
+        console.log("Successfull!");
+        console.log(result);
+        $('.cover').hide();
+        $('.train-errors').hide();
+        $('.train-success').show();
+      },
+      error: function (request) {
+        console.log("Something goes wrong, try again!", request);
+
+        console.log(request);
+        render.errorBlock(request);
+
+        $('.cover').hide();
+      }
+    })
   }
 }
