a085a641c1f8637de157fe515f6cdb27f69896a5
Szymon Miks <szymon.miks@netguru.com>
2019-09-08 23:09:47 +0200

    [trello-16]: Added report column to model and model properties

--- app/apmc/models.py
+++ app/apmc/models.py
@@ -1,10 +1,12 @@
 import datetime
+import json
 import os
 
 from flask import current_app
 
 from app.apmc.config import APMC_DATASET_UPLOAD_PATH
 from app.apmc.config import APMC_MODELS_UPLOAD_PATH
+from app.apmc.ds.common.constants import RANDOM_FOREST_NAMES
 from app.database import db
 
 
@@ -19,6 +21,7 @@ class APMCData(db.Model):
     normalization = db.Column(db.Boolean, default=False)
     trained_model = db.Column(db.String(40), nullable=True)
     model_metrics = db.Column(db.Text, nullable=True)
+    report = db.Column(db.String(40), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
     def dataset_path(self):
@@ -29,6 +32,18 @@ class APMCData(db.Model):
             return os.path.join(current_app.root_path, APMC_MODELS_UPLOAD_PATH, self.trained_model)
 
         return None
+
+    @property
+    def model_name(self):
+        if self.model_metrics:
+            model_metrics = json.loads(self.model_metrics)
+            return model_metrics.get('model_name', None)
+
+        return None
+
+    @property
+    def has_tree_graph(self):
+        return self.model_name in RANDOM_FOREST_NAMES
 
     def __repr__(self):
         return f"APMC data: {self.project_name}, {self.model_type}"
