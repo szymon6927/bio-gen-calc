1f85739beac05c909951f9d790c6ee63687b5991
Szymon Miks <szymon.miks@netguru.com>
2019-09-15 19:15:53 +0200

    [trello-71]: Added tests for APMC GET views

--- tests/integration/test_apmc.py
+++ tests/integration/test_apmc.py
@@ -1,9 +1,43 @@
 from tests.integration.constants import URL
+from tests.integration.utils import create_customer
+from tests.integration.utils import login_customer
 
 
-def test_get_about_ok(test_client):
-    response = test_client.get(URL.ABOUT_GET)
+def test_get_apmc_ok(test_client):
+    response = test_client.get(URL.APMC_GET)
+
     assert response.status_code == 200
-    assert b'About' in response.data
-    assert b'Szymon Miks' in response.data
-    assert b'Jan' in response.data
+    assert b'APMC' in response.data
+
+
+def test_get_apmc_as_not_logged_in_user(test_client):
+    response = test_client.get(URL.APMC_GET)
+
+    assert response.status_code == 200
+    assert b'You have to be logged in' in response.data
+
+
+def test_get_apmc_as_logged_in_user(test_client):
+    customer, plain_password = create_customer()
+    login_customer(test_client, customer.login, plain_password)
+
+    response = test_client.get(URL.APMC_GET)
+
+    assert response.status_code == 200
+    assert b'Your project name' in response.data
+    assert b'Click to upload file' in response.data
+    assert b'Pre-train' in response.data
+
+
+def test_get_apmc_how_to_use_ok(test_client):
+    response = test_client.get(URL.APMC_HOW_TO_USE_GET)
+
+    assert response.status_code == 200
+    assert b'How to use' in response.data
+
+
+def test_get_apmc_documentation_ok(test_client):
+    response = test_client.get(URL.APMC_DOCUMENTATION_GET)
+
+    assert response.status_code == 200
+    assert b'Documentation' in response.data
