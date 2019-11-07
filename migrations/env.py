d0f931de20d4e9d4b51bdbcbfed7112261b0bd89
Szymon Miks <szymon.miks@netguru.com>
2019-11-07 20:28:06 +0100

    [trello-108]: Added migrations, and changed alembic config compare_type=False

--- migrations/env.py
+++ migrations/env.py
@@ -44,7 +44,8 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, compare_type=True)
+    # context.configure(url=url, compare_type=True)
+    context.configure(url=url)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -73,11 +74,17 @@ def run_migrations_online():
     )
 
     connection = engine.connect()
+    # context.configure(
+    #     connection=connection,
+    #     target_metadata=target_metadata,
+    #     process_revision_directives=process_revision_directives,
+    #     compare_type=True,
+    #     **current_app.extensions['migrate'].configure_args,
+    # )
     context.configure(
         connection=connection,
         target_metadata=target_metadata,
         process_revision_directives=process_revision_directives,
-        compare_type=True,
         **current_app.extensions['migrate'].configure_args,
     )
 
